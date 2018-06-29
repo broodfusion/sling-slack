9303544c9c725331d5ad8642063991f48d3bb8c9
Shawn Liang <sliang@Shawns-MacBook-Pro.local>
2018-06-29 09:47:03 -0700

    add password hashing

---
+++ api/lib/sling/accounts/user.ex
@@ -0,0 +1,40 @@
+defmodule Sling.Accounts.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do
+    field(:email, :string)
+    field(:password_hash, :string)
+    field(:username, :string)
+    field(:password, :string, virtual: true)
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(user, attrs \\ %{}) do
+    user
+    |> cast(attrs, [:username, :email])
+    |> validate_required([:username, :email])
+    |> unique_constraint(:username)
+    |> unique_constraint(:email)
+  end
+
+  def registration_changeset(user, params) do
+    user
+    |> changeset(params)
+    |> cast(params, [:password])
+    |> validate_length(:password, min: 6, max: 100)
+    |> put_password_hash()
+  end
+
+  def put_password_hash(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: password}} ->
+        put_change(changeset, :password_hash, Comeonin.Argon2.hashpwsalt(password))
+
+      _ ->
+        changeset
+    end
+  end
+end
